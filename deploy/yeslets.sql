ad9e9890a78034c32735b0c7cb5ce54d396c41a2
Libby Horacek <libby@positiondev.com>
2015-10-15 21:22:01 -0400

    Changed schema.

--- deploy/yeslets.sql
+++ deploy/yeslets.sql
@@ -8,19 +8,28 @@ CREATE TABLE users
   name text NOT NULL,
   email text NOT NULL,
   password text NOT NULL,
-  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP );
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+ );
 
 CREATE TABLE signals (
   id serial NOT NULL primary key,
-  user_id integer NOT NULL REFERENCES users(id),
+  user text NOT NULL,
   action text NOT NULL,
   topic text NOT NULL,
-  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP );
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT user_fkey FOREIGN KEY (user)
+    REFERENCES user (name) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+);
 
 CREATE TABLE yesletses (
   id serial NOT NULL PRIMARY KEY,
   signal_id integer NOT NULL REFERENCES signals(id),
-  user_id integer NOT NULL REFERENCES users(id),
-  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP );
+  user text NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT user_fkey FOREIGN KEY (user)
+    REFERENCES user (name) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+);
 
 COMMIT;
